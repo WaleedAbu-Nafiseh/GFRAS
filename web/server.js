4340c814f5d6bfc9b9bc225ec8de68b6d739cd53
WaleedAbu-Nafiseh <43027952+WaleedAbu-Nafiseh@users.noreply.github.com>
2020-07-04 13:25:34 +0300

    Update server.js

--- web/server.js
+++ web/server.js
@@ -8,25 +8,11 @@ var mongoose=require('mongoose')
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-var dburl='mongodb+srv://user:user@cluster0.h9dla.mongodb.net/<dbname>?retryWrites=true&w=majority'
-var messages=[];
 
-app.get('/messages',(req,res)=>{
-    res.send(messages);
-});
+var server= http.listen(3000,()=>{
+    console.log('server is on the '+server.address().port);
+   });
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'./login/login.html'));
-});
-app.post('/messages',(req,res)=>{
-    console.log(req.body)
-    messages.push(req.body)
-    io.emit('message',req.body);
-
-    res.sendStatus(200);
-});
-var server= http.listen(3000,()=>{
- console.log('server is on the '+server.address().port);
-});
-io.on('connecion',(socket)=>{
-    console.log("User connected")
 });
