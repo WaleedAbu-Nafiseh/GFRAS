d5cc29d6672a4babc1077bcd46c673686e237fbd
WaleedAbu-Nafiseh <43027952+WaleedAbu-Nafiseh@users.noreply.github.com>
2020-07-10 00:25:20 +0300

    Update set up

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ var bodyParser= require('body-parser');
 var app = express();
 var path=require('path');
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose=require('mongoose')
+import apiRouter from './api/index';
+import  mongoose from 'mongoose';
 app.use(express.static(__dirname));
+
+//Mongose connection 
+mongoose.Promise= global.Promise;
+mongoose.connect('mongodb://localhost/EtihadDB',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+// bodyparser
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
 
 var server= http.listen(3000,()=>{
     console.log('server is on the '+server.address().port);
-   });
+});
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'./login/login.html'));
 });
+
+app.use('/api',apiRouter);
