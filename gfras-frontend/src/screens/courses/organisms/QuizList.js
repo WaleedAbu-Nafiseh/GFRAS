db1c42a81650b380d62a473345bfe3a89ebe424b
AliAbuAlRub <aliabuelrub1@hotmail.com>
2020-09-26 21:12:40 +0300

    Adding create quiz to quiz list if there is no quizzes

--- gfras-frontend/src/screens/courses/organisms/QuizList.js
+++ gfras-frontend/src/screens/courses/organisms/QuizList.js
@@ -4,11 +4,13 @@ import { useQuizContext } from '../QuizContext';
 import { useIntl } from 'react-intl';
 import { startQuiz } from '../../../API/quizzes/startQuiz';
 import { useLocation } from 'react-router-dom';
+import { useCoursesContext } from '../CoursesContext';
 
 export function QuizList() {
 	const { pathname } = useLocation();
 	const { formatMessage } = useIntl();
-	const { data } = useQuizContext();
+	const { data, setIsSideMenuExpanded } = useQuizContext();
+	const { setSelectedCourseDetail } = useCoursesContext();
 
 	if (!data) {
 		return <Spinner />;
@@ -18,6 +20,34 @@ export function QuizList() {
 		await startQuiz({ docID: quizID });
 		window.location.href = `${pathname}/${quizID}`;
 	};
+
+	if (data.length === 0) {
+		return (
+			<Flex
+				w='full'
+				h='full'
+				direction='column'
+				align='center'
+				justify='center'
+			>
+				<Text fontSize={20} fontWeight={500}>
+					{formatMessage({ id: 'course.quiz.quizList.noQuizzes' })}
+				</Text>
+				<Button
+					w='fit-content'
+					bg='#3182ce'
+					mt='10px'
+					_hover={{ bg: '#006ace' }}
+					onClick={() => {
+						setIsSideMenuExpanded(true);
+						setSelectedCourseDetail('create-quiz');
+					}}
+				>
+					{formatMessage({ id: 'course.quiz.quizList.createNewQuiz' })}
+				</Button>
+			</Flex>
+		);
+	}
 
 	return (
 		<Flex direction='column' p='100px' w='full'>
