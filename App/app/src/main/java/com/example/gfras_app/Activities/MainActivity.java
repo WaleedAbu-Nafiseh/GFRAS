ae72d434badb79e58e18108c98cd36e5e461ed1d
WaleedAbu-Nafiseh <>
2020-11-01 17:12:34 +0200

    Fixed FR

--- App/app/src/main/java/com/example/gfras_app/Activities/MainActivity.java
+++ App/app/src/main/java/com/example/gfras_app/Activities/MainActivity.java
@@ -6,34 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.gfras_app.Activities.Instructor.InstructorCoursesList;
+import com.example.gfras_app.Activities.Instructor.InstructorHomePageActivity;
 import com.example.gfras_app.R;
 import pp.facerecognizer.FirstFRActicity;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogIn;
     Button btnSignup;
-
+    TextView areYouInstructor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
         btnLogIn = findViewById(R.id.btnLogIn);
         btnSignup = findViewById(R.id.btnSignup);
-
-        btnLogIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-
-            }
+        areYouInstructor=findViewById(R.id.areYouInstructor);
+        areYouInstructor.setText(getString(R.string.areYouInstructor));
+        areYouInstructor.setOnClickListener(v->{
+            Intent i = new Intent(getApplicationContext(), InstructorCoursesList.class);
+            startActivity(i);
+        });
+        btnLogIn.setOnClickListener(v-> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
         });
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), FirstFRActicity.class);
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivity(intent);
 
             }
