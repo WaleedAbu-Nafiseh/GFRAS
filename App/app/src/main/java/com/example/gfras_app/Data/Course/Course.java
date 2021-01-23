8e6b2330dd5462b7492a7e22df1391c48e3fff9b
WaleedAbu-Nafiseh <>
2021-01-23 21:41:05 +0200

    updated FR with the AttendanceObject

--- App/app/src/main/java/com/example/gfras_app/Data/Course/Course.java
+++ App/app/src/main/java/com/example/gfras_app/Data/Course/Course.java
@@ -16,17 +16,26 @@ public class Course {
     String InstructorID;
     List<String> quizzes;
     List<String> students;
+    HashMap<String,ArrayList<AttendanceItem>> attendance;
 
-    public Course(String courseId, String courseName, String instructorID, List<String> quizzes, List<String> students) {
+    public Course(String courseId, String courseName, String instructorID, List<String> quizzes, List<String> students,HashMap<String,ArrayList<AttendanceItem>>  attendance) {
         this.courseId = courseId;
         this.courseName = courseName;
         this.InstructorID = instructorID;
         this.quizzes = quizzes;
         this.students = students;
-
+        this.attendance=attendance;
     }
 
     public Course() {
+    }
+
+    public HashMap<String,ArrayList<AttendanceItem>>  getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(HashMap<String,ArrayList<AttendanceItem>>  attendance) {
+        this.attendance = attendance;
     }
 
     public String getCourseId() {
